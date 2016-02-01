7466c59cab90e15089044bd21f67db87676554bc
freder <freder.code@gmail.com>
2016-02-01 11:47:42 +0100

    attacker profiles preset data

---
+++ app/data/attacker-profiles.js
@@ -0,0 +1,339 @@
+module.exports =
+[
+	{
+		"title": "Employee Reckless",
+		"intent": "non-hostile",
+		"access": "internal",
+		"outcome": [
+			"damage",
+			"embarrassment"
+		],
+		"limits": "legal",
+		"resources": "individual",
+		"skills": "adept",
+		"objective": [
+			"all of the above"
+		],
+		"visibility": "covert"
+	},
+	{
+		"title": "Employee Untrained",
+		"intent": "non-hostile",
+		"access": "internal",
+		"outcome": [
+			"damage",
+			"embarrassment"
+		],
+		"limits": "code of conduct",
+		"resources": "individual",
+		"skills": "minimal",
+		"objective": [
+			"all of the above"
+		],
+		"visibility": "overt"
+	},
+	{
+		"title": "Info Partner",
+		"intent": "non-hostile",
+		"access": "internal",
+		"outcome": [
+			"damage",
+			"embarrassment"
+		],
+		"limits": "code of conduct",
+		"resources": "individual",
+		"skills": "operational",
+		"objective": [
+			"all of the above"
+		],
+		"visibility": "clandestine"
+	},
+	{
+		"title": "Anarchist",
+		"intent": "hostile",
+		"access": "external",
+		"outcome": [
+			"damage"
+		],
+		"limits": "extra-legal, major",
+		"resources": "club",
+		"skills": "none",
+		"objective": [
+			"destroy"
+		],
+		"visibility": "overt"
+	},
+	{
+		"title": "Civil Activist",
+		"intent": "hostile",
+		"access": "external",
+		"outcome": [
+			"embarrassment"
+		],
+		"limits": "extra-legal, minor",
+		"resources": "organization",
+		"skills": "adept",
+		"objective": [
+			"copy"
+		],
+		"visibility": "covert"
+	},
+	{
+		"title": "Competitor",
+		"intent": "hostile",
+		"access": "external",
+		"outcome": [
+			"business advantage",
+			"tech advantage"
+		],
+		"limits": "extra-legal, minor",
+		"resources": "organization",
+		"skills": "adept",
+		"objective": [
+			"copy"
+		],
+		"visibility": "clandestine"
+	},
+	{
+		"title": "Corrupt Government Official",
+		"intent": "hostile",
+		"access": "external",
+		"outcome": [
+			"business advantage",
+			"tech advantage"
+		],
+		"limits": "extra-legal, minor",
+		"resources": "government",
+		"skills": "adept",
+		"objective": [
+			"deny"
+		],
+		"visibility": "overt"
+	},
+	{
+		"title": "Data Miner",
+		"intent": "hostile",
+		"access": "external",
+		"outcome": [
+			"business advantage",
+			"tech advantage"
+		],
+		"limits": "extra-legal, minor",
+		"resources": "team",
+		"skills": "adept",
+		"objective": [
+			"copy"
+		],
+		"visibility": "clandestine"
+	},
+	{
+		"title": "Employee Disgruntled",
+		"intent": "hostile",
+		"access": "internal",
+		"outcome": [
+			"damage",
+			"embarrassment"
+		],
+		"limits": "extra-legal, major",
+		"resources": "individual",
+		"skills": "operational",
+		"objective": [
+			"destroy",
+			"damage"
+		],
+		"visibility": "multiple"
+	},
+	{
+		"title": "Government Cyberwarrior",
+		"intent": "hostile",
+		"access": "external",
+		"outcome": [
+			"damage",
+			"embarrassment"
+		],
+		"limits": "extra-legal, major",
+		"resources": "government",
+		"skills": "adept",
+		"objective": [
+			"deny",
+			"destroy",
+			"damage"
+		],
+		"visibility": "multiple"
+	},
+	{
+		"title": "Government Spy",
+		"intent": "hostile",
+		"access": "internal",
+		"outcome": [
+			"business advantage",
+			"tech advantage"
+		],
+		"limits": "extra-legal, major",
+		"resources": "government",
+		"skills": "adept",
+		"objective": [
+			"copy"
+		],
+		"visibility": "multiple"
+	},
+	{
+		"title": "Internal Spy",
+		"intent": "hostile",
+		"access": "internal",
+		"outcome": [
+			"acquisition / theft",
+			"tech advantage"
+		],
+		"limits": "extra-legal, minor",
+		"resources": "organization",
+		"skills": "adept",
+		"objective": [
+			"copy"
+		],
+		"visibility": "clandestine"
+	},
+	{
+		"title": "Irrational Individual",
+		"intent": "hostile",
+		"access": "external",
+		"outcome": [
+			"damage",
+			"embarrassment"
+		],
+		"limits": "extra-legal, major",
+		"resources": "individual",
+		"skills": "none",
+		"objective": [
+			"all of the above"
+		],
+		"visibility": "multiple"
+	},
+	{
+		"title": "Legal Adversary",
+		"intent": "hostile",
+		"access": "external",
+		"outcome": [
+			"business advantage",
+			"embarrassment"
+		],
+		"limits": "legal",
+		"resources": "organization",
+		"skills": "adept",
+		"objective": [
+			"copy",
+			"take"
+		],
+		"visibility": "overt"
+	},
+	{
+		"title": "Mobster",
+		"intent": "hostile",
+		"access": "external",
+		"outcome": [
+			"acquisition / theft"
+		],
+		"limits": "extra-legal, major",
+		"resources": "organization",
+		"skills": "adept",
+		"objective": [
+			"take"
+		],
+		"visibility": "covert"
+	},
+	{
+		"title": "Radical Activist",
+		"intent": "hostile",
+		"access": "external",
+		"outcome": [
+			"damage",
+			"embarrassment"
+		],
+		"limits": "extra-legal, minor",
+		"resources": "organization",
+		"skills": "adept",
+		"objective": [
+			"all of the above"
+		],
+		"visibility": "overt"
+	},
+	{
+		"title": "Sensationalist",
+		"intent": "hostile",
+		"access": "external",
+		"outcome": [
+			"damage",
+			"embarrassment"
+		],
+		"limits": "extra-legal, minor",
+		"resources": "club",
+		"skills": "minimal",
+		"objective": [
+			"all of the above"
+		],
+		"visibility": "overt"
+	},
+	{
+		"title": "Terrorist",
+		"intent": "hostile",
+		"access": "external",
+		"outcome": [
+			"damage"
+		],
+		"limits": "extra-legal, major",
+		"resources": "organization",
+		"skills": "adept",
+		"objective": [
+			"destroy",
+			"damage",
+			"take"
+		],
+		"visibility": "covert"
+	},
+	{
+		"title": "Thief",
+		"intent": "hostile",
+		"access": "internal",
+		"outcome": [
+			"acquisition / theft"
+		],
+		"limits": "extra-legal, minor",
+		"resources": "individual",
+		"skills": "none",
+		"objective": [
+			"take"
+		],
+		"visibility": "clandestine"
+	},
+	{
+		"title": "Vandal",
+		"intent": "hostile",
+		"access": "external",
+		"outcome": [
+			"damage"
+		],
+		"limits": "extra-legal, minor",
+		"resources": "contest",
+		"skills": "operational",
+		"objective": [
+			"all of the above"
+		],
+		"visibility": "covert"
+	},
+	{
+		"title": "Vendor",
+		"intent": "hostile",
+		"access": "internal",
+		"outcome": [
+			"business advantage",
+			"tech advantage"
+		],
+		"limits": "legal",
+		"resources": "team",
+		"skills": "operational",
+		"objective": [
+			"copy"
+		],
+		"visibility": "clandestine"
+	}
+];
