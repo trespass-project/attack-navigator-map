97804e6164f21f55fa7f745148f95efc6fc37ec3
freder <freder.code@gmail.com>
2016-06-22 14:27:17 +0200

    App component in separate file

---
+++ app/scripts/App.js
@@ -0,0 +1,154 @@
+const React = require('react');
+
+const fs = require('fs');
+const pkg = JSON.parse(fs.readFileSync('./package.json').toString());
+
+const actionCreators = require('./actionCreators.js');
+const knowledgebaseApi = require('trespass.js').api.knowledgebase;
+
+// const ModelDebugView = require('./components/ModelDebugView/ModelDebugView.js');
+// const MainMenu = require('./MainMenu.js');
+const Wizard = require('./Wizard.js');
+const UsageHint = require('./UsageHint.js');
+const GraphEditor = require('./GraphEditor.js');
+
+
+module.exports =
+React.createClass({
+	propTypes: {
+		dispatch: React.PropTypes.func.isRequired,
+	},
+
+	childContextTypes: {
+		theme: React.PropTypes.object,
+		dispatch: React.PropTypes.func,
+	},
+
+	getChildContext() {
+		const props = this.props;
+		return {
+			theme: props.theme,
+			dispatch: props.dispatch,
+		};
+	},
+
+	componentDidMount() {
+		const props = this.props;
+
+		props.dispatch( actionCreators.getRecentFiles() );
+
+		const editorElem = document.querySelector('#editor > svg');
+		props.dispatch( actionCreators.setEditorElem(editorElem) );
+
+		window.addEventListener('beforeunload', this.handleBeforeUnload);
+		window.addEventListener('keydown', this.saveHandler);
+	},
+
+	componentWillUnmount() {
+		window.removeEventListener('beforeunload', this.handleBeforeUnload);
+		window.removeEventListener('keydown', this.saveHandler);
+	},
+
+	handleBeforeUnload(event) {
+		event.preventDefault();
+
+		// TODO: check if model is empty
+		// if so, delete from knowledgebase
+
+		const msg = 'Are you sure?';
+		event.returnValue = msg;
+		return msg;
+	},
+
+	saveHandler(event) {
+		// if control or command key is pressed and the s key is pressed
+		if ((event.ctrlKey || event.metaKey) && event.keyCode === 83) {
+			event.preventDefault();
+			this.props.dispatch( actionCreators.saveModelToKb() );
+		}
+	},
+
+	fetchKbData(event) {
+		event.preventDefault();
+		this.props.dispatch( actionCreators.fetchKbData() );
+	},
+
+	render() {
+		const props = this.props;
+		return (
+			<div id='container'>
+				<input type='file' accept='.svg' id='add-file' />
+
+				<div id='meta'>
+					{(props.metadata.id)
+						? <div>
+							<div>model id: {props.metadata.id}</div>
+							<div>title: {props.metadata.title}</div>
+						</div>
+						: ''
+					}
+
+					{(props.metadata.id)
+						? <div>
+							<a
+								href={`${knowledgebaseApi.host}tkb/files/edit?model_id=${props.metadata.id}`}
+								target='_blank'
+							>
+								edit knowledgebase files
+							</a>
+							<br />
+							<a
+								href='#'
+								onClick={this.fetchKbData}
+							>
+								re-fetch knowledgebase data
+							</a>
+						</div>
+						: ''
+					}
+
+					<hr style={{ marginTop: 5, marginBottom: 5 }} />
+					ANM {pkg.version}<br />
+					<UsageHint>
+						<a
+							target='_blank'
+							href='https://docs.google.com/document/d/1Qp8nJgdvDespq1Q5zQcAT1SSTKK23m2XKMUKKmoKYQU/edit?usp=sharing'
+						>
+							manual
+						</a>
+					</UsageHint>
+
+					<UsageHint>
+						<a
+							href='https://gitlab.com/freder/anm-feedback/issues'
+							target='_blank'
+						>
+							report bug / give feedback
+						</a>
+					</UsageHint>
+				</div>
+
+				<div id='map-container'>
+					<div id='map'>
+						<GraphEditor id='editor' {...props} />
+					</div>
+				</div>
+
+				{/*<div id='model-debug-view'>
+					<div className='panel-section'>
+						<h3 className='title'>debug</h3>
+						<MainMenu id='main-menu' {...props} />
+					</div>
+					<div className='panel-section'>
+						<h3 className='title'>model</h3>
+						<ModelDebugView {...props} />
+					</div>
+				</div>*/}
+
+				<div id='panel-container'>
+					<Wizard {...props} />
+				</div>
+			</div>
+		);
+	}
+});
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,176 +1,35 @@
-const React = require('react');
-const reactDOM = require('react-dom');
 const _ = require('lodash');
 const R = require('ramda');
-const createStore = require('redux').createStore;
-const combineReducers = require('redux').combineReducers;
-const applyMiddleware = require('redux').applyMiddleware;
-const connect = require('react-redux').connect;
-const Provider = require('react-redux').Provider;
-const thunk = require('redux-thunk');
 
-const fs = require('fs');
-const pkg = JSON.parse(fs.readFileSync('./package.json').toString());
+const React = require('react');
+const reactDOM = require('react-dom');
+
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider, connect } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
 
 const modelReducer = require('./modelReducer.js');
 const interfaceReducer = require('./interfaceReducer.js');
-
-const knowledgebaseApi = require('trespass.js').api.knowledgebase;
-
-// const ModelDebugView = require('./components/ModelDebugView/ModelDebugView.js');
-// const MainMenu = require('./MainMenu.js');
-const Wizard = require('./Wizard.js');
-const UsageHint = require('./UsageHint.js');
-
-const GraphEditor = require('./GraphEditor.js');
-const actionCreators = require('./actionCreators.js');
 
 const HTML5Backend = require('react-dnd-html5-backend');
 const DragDropContext = require('react-dnd').DragDropContext;
 
 
-let App = React.createClass({
-	childContextTypes: {
-		theme: React.PropTypes.object,
-		dispatch: React.PropTypes.func,
-	},
+function configureStore(initialState) {
+	const combinedReducers = combineReducers({
+		model: modelReducer,
+		interface: interfaceReducer,
+	});
 
-	getChildContext() {
-		const props = this.props;
-		return {
-			theme: props.theme,
-			dispatch: props.dispatch,
-		};
-	},
+	const store = createStore(
+		combinedReducers,
+		initialState,
+		applyMiddleware(ReduxThunk)
+	);
 
-	componentDidMount() {
-		const props = this.props;
-
-		props.dispatch( actionCreators.getRecentFiles() );
-
-		const editorElem = document.querySelector('#editor > svg');
-		props.dispatch( actionCreators.setEditorElem(editorElem) );
-
-		window.addEventListener('beforeunload', this.handleBeforeUnload);
-		window.addEventListener('keydown', this.saveHandler);
-	},
-
-	componentWillUnmount() {
-		window.removeEventListener('beforeunload', this.handleBeforeUnload);
-		window.removeEventListener('keydown', this.saveHandler);
-	},
-
-	handleBeforeUnload(event) {
-		event.preventDefault();
-
-		// TODO: check if model is empty
-		// if so, delete from knowledgebase
-
-		const msg = 'Are you sure?';
-		event.returnValue = msg;
-		return msg;
-	},
-
-	saveHandler(event) {
-		// if control or command key is pressed and the s key is pressed
-		if ((event.ctrlKey || event.metaKey) && event.keyCode === 83) {
-			event.preventDefault();
-			this.props.dispatch( actionCreators.saveModelToKb() );
-		}
-	},
-
-	fetchKbData(event) {
-		event.preventDefault();
-		this.props.dispatch( actionCreators.fetchKbData() );
-	},
-
-	render() {
-		const props = this.props;
-		return (
-			<div id='container'>
-				<input type='file' accept='.svg' id='add-file' />
-
-				<div id='meta'>
-					{(props.metadata.id)
-						? <div>
-							<div>model id: {props.metadata.id}</div>
-							<div>title: {props.metadata.title}</div>
-						</div>
-						: ''
-					}
-
-					{(props.metadata.id)
-						? <div>
-							<a
-								href={`${knowledgebaseApi.host}tkb/files/edit?model_id=${props.metadata.id}`}
-								target='_blank'
-							>
-								edit knowledgebase files
-							</a>
-							<br />
-							<a
-								href='#'
-								onClick={this.fetchKbData}
-							>
-								re-fetch knowledgebase data
-							</a>
-						</div>
-						: ''
-					}
-
-					<hr style={{ marginTop: 5, marginBottom: 5 }} />
-					ANM {pkg.version}<br />
-					<UsageHint>
-						<a
-							target='_blank'
-							href='https://docs.google.com/document/d/1Qp8nJgdvDespq1Q5zQcAT1SSTKK23m2XKMUKKmoKYQU/edit?usp=sharing'
-						>
-							manual
-						</a>
-					</UsageHint>
-
-					<UsageHint>
-						<a
-							href='https://gitlab.com/freder/anm-feedback/issues'
-							target='_blank'
-						>
-							report bug / give feedback
-						</a>
-					</UsageHint>
-				</div>
-
-				<div id='map-container'>
-					<div id='map'>
-						<GraphEditor id='editor' {...props} />
-					</div>
-				</div>
-
-				{/*<div id='model-debug-view'>
-					<div className='panel-section'>
-						<h3 className='title'>debug</h3>
-						<MainMenu id='main-menu' {...props} />
-					</div>
-					<div className='panel-section'>
-						<h3 className='title'>model</h3>
-						<ModelDebugView {...props} />
-					</div>
-				</div>*/}
-
-				<div id='panel-container'>
-					<Wizard {...props} />
-				</div>
-			</div>
-		);
-	}
-});
-
-
-const reducer = combineReducers({
-	model: modelReducer,
-	interface: interfaceReducer,
-});
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
-const store = createStoreWithMiddleware(reducer);
+	return store;
+}
+const store = configureStore();
 
 function mapStateToProps(state) {
 	// flatten one level
@@ -180,6 +39,7 @@ function mapStateToProps(state) {
 	);
 }
 
+let App = require('./App.js');
 App = DragDropContext(HTML5Backend)(App); // eslint-disable-line new-cap
 App = connect(mapStateToProps)(App);
 
